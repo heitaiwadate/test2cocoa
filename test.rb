c09cae7b61eca8cce4845d73dd89b80f82486127
cocoa1225 <35025624+cocoa1225@users.noreply.github.com>
2018-02-06 23:25:12 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -155,4 +155,62 @@ bot.command (:taiki) do |event|
         event.send_message"これをやりましょう「#{array[ransu]}」"
       end
 
+bot.command (:test) do |event,*code|
+  name = code[0]
+
+url = "https://dak.gg/profile/#{name}"
+
+charset = nil
+html = open(url).read do |f|
+  charset = f.charset # 文字種別を取得
+  f.read # htmlを読み込んで変数htmlに渡す
+end
+# htmlをパース(解析)してオブジェクトを生成
+page = Nokogiri::HTML.parse(html, nil, charset)
+pubgrate = page.search('img.grade-icon')
+
+File.open('file.txt','w') do |f|
+  f.puts("#{pubgrate}")
+end
+
+File.open('file.txt','r').each do |r|
+@bronze = r.include?("BRONZE")
+@silver= r.include?("SILVER")
+@gold = r.include?("GOLD")
+@platinum = r.include?("PLATINUM")
+@diamond = r.include?("DIAMOND")
+@master = r.include?("MASTER")
+@grand = r.include?("GRANDMASTER")
+@top500 = r.include?("TOP500")
+end
+
+if @bronze
+  event.send_message"あなたのレートはブロンズ帯です"
+
+elsif @silver
+  event.send_message"あなたのレートはシルバー帯です"
+
+elsif @gold
+  event.send_message"あなたのレートはゴールド帯です"
+
+elsif @platinum
+  event.send_message"あなたのレートはプラチナ帯です"
+
+elsif @diamond
+  event.send_message"あなたのレートはダイアモンド帯です"
+
+elsif @grand
+  event.send_message"あなたのレートはグランドマスター帯です"
+
+elsif @top500
+  event.send_message"あなたのレートはTOP500位です"
+
+elsif @master
+  event.send_message"あなたのレートはマスター帯です"
+
+else
+  event.send_message"ランク帯が取得できなかった可能性が高いです"
+end
+end
+
 bot.run
